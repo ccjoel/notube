a44b71f433e3dfcc730cd9eec577238cb5d4a346
Joel Quiles <quilesbaker@gmail.com>
2017-08-30 19:16:58 -0400

    removing some useless TODO

--- src/thehex/notube/cli.clj
+++ src/thehex/notube/cli.clj
@@ -25,7 +25,6 @@
    ;; A boolean option defaulting to nil
    ["-h" "--help"]])
 
-;; TODO: doesnt need to be the main app, could be a fn that core.clj calls
 (defn -main [& args]
   (let [clargs (parse-opts args cli-options)
         opts (:options clargs)]
